8bee9bb0f736d5bddbbc51d1a1a215f51880d86e
andrew-anlu <llanc@isoftstone.com>
2014-08-21 13:35:14 +0800

    commit

---
+++ GraphDemo1/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// HockeyKit
+#define COCOAPODS_POD_AVAILABLE_HockeyKit
+#define COCOAPODS_VERSION_MAJOR_HockeyKit 2
+#define COCOAPODS_VERSION_MINOR_HockeyKit 0
+#define COCOAPODS_VERSION_PATCH_HockeyKit 7
+
